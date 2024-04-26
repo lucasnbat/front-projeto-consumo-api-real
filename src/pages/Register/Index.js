fdbf8d6ac61a24bc92d26da1364f9b0c3a3feeea
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-04-26 17:16:31 -0400

    commit

--- src/pages/Register/Index.js
+++ src/pages/Register/Index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 // esse é o cara que dispara as ações
 // as ações mandam o que vai ser feito de alteração
 // elas tem um tipo, payload, igual o que vimos na rocketseat
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Register() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <Container>
-      <h1>Register</h1>
+      <h1>Crie sua conta</h1>
+
+      <Form>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu nome"
+          />
+        </label>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu e-mail"
+          />
+        </label>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Sua senha"
+          />
+        </label>
+      </Form>
     </Container>
   );
 }
--- src/pages/Register/styled.js
+++ src/pages/Register/styled.js
@@ -3,3 +3,4 @@ import styled from 'styled-components';
 export const Paragraph = styled.p`
   font-size: 40px;
 `;
+export const Form = styled.form``;
